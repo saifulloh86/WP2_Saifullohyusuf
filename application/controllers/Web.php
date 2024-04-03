d3f166310f01879e95ddd94788318d588a0a7f54
saifulloh86 <saifulloh4704@gmail.com>
2024-04-03 10:12:36 +0700

    menambahkan contoh 5 dan pertemuan 4

---
+++ application/controllers/Web.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('nodirectscriptaccessallowed');
+
+
+class Web extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+    public function index()
+    {
+        $data['judul'] = "HalamanDepan";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_index', $data);
+        $this->load->view('v_footer', $data);
+    }
+    public function about()
+    {
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_about', $data);
+        $this->load->view('v_footer', $data);
+    }
+}
